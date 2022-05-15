30b8912e84caa87edc91e0f9b16ca3dba9cac0f4
Elias Müller <elias.mr1@gmail.com>
2022-05-15 09:14:07 +0200

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,11 +12,13 @@ alias restart='sudo restart now'
 
 alias config="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
 
-alias ls='exa -l --color=always --group-directories-first'        # 
+alias ls='exa -l --color=always --group-directories-first'        #
 alias ll='exa -al --color=always --group-directories-first'       # long format
 alias lt='exa -aT --color=always --group-directories-first'       # tree listing
 alias l.='exa -ld .?* --color=always --group-directories-first'   # only hidden files and dirs
 alias ldot=l.
+
+alias qmltestrunner=qmltestrunner-qt6
 
 set fish_greeting
 set -g fish_prompt_pwd_dir_length 0
